9f1b32c0e26028fd78cf75cf94e3d8352c6c066e
midnightCODA <mbunjimo@gmail.com>
2024-06-19 13:24:33 +0300

    carousel

--- src/components/HomeCarousel.jsx
+++ src/components/HomeCarousel.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useRef } from 'react';
 import { Carousel } from '@mantine/carousel';
-import { Image } from '@mantine/core';
+import { Image, Overlay, Text } from '@mantine/core';
 import president from '../assets/drsamia.jpg'
 import groupphoto from '../assets/group-photo-2.jpg'
 import utiajiwasaini from '../assets/Utiajiwasaini.jpg'
@@ -30,19 +30,52 @@ const HomeCarousel = () => {
             className='rounded-lg h-full w-full'
             src={president}
           />
-          {/* <div class="absolute inset-x-0 bg-gradient-to-t from-slate-900 via-slate-900/40 bottom-0 rounded-3xl h-56 w-full text-center">
-            <div className='absolute flex flex-col w-full text-center bottom-24'>
-              <h3 className='  text-white text-4xl font-bold '>Hello there testing new stuff</h3>
-              <p className='p-regular-20 md:p-regular-24 text-white'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, corporis consequatur? Nobis hic laborum ab nemo totam iste in at possimus unde ut, quae tempora similique, facilis natus eaque incidunt!</p>
+
+
+          <Overlay
+            gradient="linear-gradient(0deg, rgba(0, 0, 0, 0.80) 0%, rgba(0, 0, 0, 0) 100%)"
+            opacity={0.85}
+            radius="xl"
+            zIndex={0}
+          />
+
+          <div className='absolute bottom-0 left-0 right-0 text-center flex flex-col justify-center w-full'>
+            <div className='w-full p-8'>
+              <p className='h3-bold text-white text-center z-50'>
+                Drumbeats carnival limted
+              </p>
+              <p className='text-white p-regular-20 md:p-regular-24'>Book and learn helpful tips
+                from 3,168+ mentors in world-class companies with our global community.</p>
             </div>
-          </div> */}
+          </div>
+
+
+
         </Carousel.Slide>
-        <Carousel.Slide>
+        <Carousel.Slide className='relative'>
           <Image
             radius="xl"
             className='rounded-lg h-full w-full'
             src={groupphoto}
           />
+
+          <Overlay
+            gradient="linear-gradient(0deg, rgba(0, 0, 0, 0.80) 0%, rgba(0, 0, 0, 0) 100%)"
+            opacity={0.85}
+            radius="xl"
+            zIndex={0}
+          />
+
+          <div className='absolute bottom-0 left-0 right-0 text-center flex flex-col justify-center w-full'>
+            <div className='w-full p-8'>
+              <p className='h3-bold text-white text-center z-50'>
+                Drumbeats carnival limted
+              </p>
+              <p className='text-white p-regular-20 md:p-regular-24'>Book and learn helpful tips
+                from 3,168+ mentors in world-class companies with our global community.</p>
+            </div>
+          </div>
+
         </Carousel.Slide>
         <Carousel.Slide>
           <Image
@@ -50,6 +83,24 @@ const HomeCarousel = () => {
             className='rounded-lg h-full w-full'
             src={utiajiwasaini}
           />
+
+          <Overlay
+            gradient="linear-gradient(0deg, rgba(0, 0, 0, 0.80) 0%, rgba(0, 0, 0, 0) 100%)"
+            opacity={0.85}
+            radius="xl"
+            zIndex={0}
+          />
+
+          <div className='absolute bottom-0 left-0 right-0 text-center flex flex-col justify-center w-full'>
+            <div className='w-full p-8'>
+              <p className='h3-bold text-white text-center z-50'>
+                Drumbeats carnival limted
+              </p>
+              <p className='text-white p-regular-20 md:p-regular-24'>Book and learn helpful tips
+                from 3,168+ mentors in world-class companies with our global community.</p>
+            </div>
+          </div>
+
         </Carousel.Slide>
       </Carousel>
     </div>
