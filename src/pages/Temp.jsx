ff4679ce3d90baaaf54e5fd48267eeee4ab2779b
midnightCODA <mbunjimo@gmail.com>
2024-06-20 15:42:27 +0300

    final changes

--- src/pages/Temp.jsx
+++ src/pages/Temp.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
+import { useForm } from '@mantine/form';
 import sittingwoman from '../assets/sittingwoman.jpg';
 import axios from 'axios';
 import baselogo from '../assets/BaseLogo.png';
 import { Footer, Header } from '../components';
-import { Button, Image, Input, Select } from '@mantine/core';
+import { Button, Center, Image, Input, NumberInput, Select } from '@mantine/core';
 import { useDisclosure, useCounter } from '@mantine/hooks';
 import { Modal, Group, Text, Badge } from '@mantine/core';
 import mnwcartoon from '../assets/mnwcartoon.png';
@@ -11,24 +12,24 @@ import textdrumbeats from '../assets/textdrumbeats.png';
 
 const Temp = () => {
     const [opened, { close, open }] = useDisclosure(true);
-    const [count, { increment, decrement }] = useCounter(3, { min: 0 });
-    const [name, setName] = useState('');
-    const [phone, setPhone] = useState('');
-    const [email, setEmail] = useState('');
-    const [source, setSource] = useState('');
 
-    const handleSubmit = async ()  => {
+    const form = useForm({
+        mode: 'uncontrolled',
+        initialValues: { name: '', phone: '', email: '', source: '' },
+        validate: {
+            name: (value) => (value.length < 2 ? 'Name must have at least 2 letters' : null),
+            phone: (value) => (/^\+?[1-9]\d{1,14}$/.test(value) ? null : 'Invalid phone number'),
+            email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+            source: (value) => (value ? null : 'Source is required'),
+        },
+    });
 
-        if (!name || !phone || !email) {
-            alert('Please fill in all required fields.');
-            return;
-        }
-
+    const handleSubmit = async (values) => {
         const body = {
-            name,
-            phone,
-            email,
-            source,
+            name: values.name,
+            phone: values.phone,
+            email: values.email,
+            source: values.source,
         };
 
         // Send 'body' to the backend
@@ -64,77 +65,75 @@ const Temp = () => {
                         <div className="flex flex-col justify-center gap-8">
                             <h1 className="h1-bold">It's Media Day at Makumbusho Village</h1>
                             <p className="p-regular-20 md:p-regular-24">
-                            Our website is currently under final stages of construction. We apologize for any inconvenience and appreciate your patience.
-                                </p>
+                                Our website is currently under final stages of construction. We apologize for any inconvenience and appreciate your patience.
+                            </p>
                         </div>
                     </div>
                 </section>
             </main>
             <Footer />
 
+
             <Modal opened={opened} onClose={close} size="auto" radius="xl">
-                <div className='flex flex-col gap-3 w-full md:w-[500px] p-3'>
-                    <div className='flex flex-col w-full'>
-                        <div className='flex flex-row justify-center items-center'>
+                <div className="flex flex-col gap-3 w-full md:w-[500px] p-3">
+                    <div className="flex flex-col w-full">
+                        <div className="flex flex-row justify-center items-center">
                             <Image
                                 radius="md"
-                                className='h-[50px] w-[50px] md:h-[40px] md:w-[40px]'
+                                className="h-[50px] w-[50px] md:h-[40px] md:w-[40px]"
                                 src={mnwcartoon}
                             />
                             <Image
                                 radius="md"
-                                className='h-[50px] w-[50px] md:h-[40px] md:w-[40px]'
+                                className="h-[50px] w-[50px] md:h-[40px] md:w-[40px]"
                                 src={textdrumbeats}
                             />
                         </div>
 
-                        <p className='text-center p-2 text-lg'>
-                            Thank you for your interest in Drum Beats Carnival! Please provide your information so we can keep you updated with the latest event details exciting news.
+                        <p className="text-center p-2 text-md">
+                            Thank you for your interest in Drum Beats Carnival! Please provide your information to get latest event details & exciting news.
                         </p>
                     </div>
 
-                    <div>
-                        <div className='flex flex-col'>
-                            <p className='font-semibold text-xl ml-1'>Name:</p>
+                    <form onSubmit={form.onSubmit(handleSubmit)}>
+                        <div className="flex flex-col">
+                            <p className="font-semibold text-lg ml-1">Name:</p>
                             <Input
                                 size="lg"
                                 radius="xl"
                                 placeholder=""
-                                color='black'
-                                value={name}
+                                color="black"
                                 required
-                                onChange={(e) => setName(e.target.value)}
+                                {...form.getInputProps('name')}
                             />
                         </div>
 
-                        <div className='flex flex-col'>
-                            <p className='font-semibold text-xl ml-1 mt-1'>Phone Number:</p>
+                        <div className="flex flex-col">
+                            <p className="font-semibold text-lg ml-1 mt-1">Phone number:</p>
+                            <NumberInput
+                                size="lg"
+                                radius="xl"
+                                placeholder=""
+                                color="black"
+                                required
+                                {...form.getInputProps('phone')}
+                            />
+                        </div>
+
+                        <div className="flex flex-col">
+                            <p className="font-semibold text-lg ml-1 mt-1">Email:</p>
                             <Input
                                 size="lg"
                                 radius="xl"
                                 placeholder=""
-                                color='black'
-                                value={phone}
+                                color="black"
                                 required
-                                onChange={(e) => setPhone(e.target.value)}
+                                {...form.getInputProps('email')}
                             />
                         </div>
 
-                        <div className='flex flex-col'>
-                            <p className='font-semibold text-xl ml-1 mt-1'>Email:</p>
-                            <Input
-                                size="lg"
-                                radius="xl"
-                                placeholder=""
-                                color='black'
-                                value={email}
-                                required
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </div>
-
-                        <div className='flex flex-col'>
-                            <p className='font-semibold text-xl ml-1 mt-1'>Where did you hear about us?</p>
+                        <div className="flex flex-col">
+                            <p className="font-semibold text-lg ml-1 mt-1">Where did you hear about us?</p>
                             <Select
                                 size="lg"
                                 radius="xl"
@@ -142,28 +141,38 @@ const Temp = () => {
                                 searchable
                                 data={[
                                     'Instagram',
+                                    "Newspaper or Magazine",
                                     'Facebook',
                                     'Friends or Family',
+                                    'Other'
                                 ]}
-                                value={source}
-                                onChange={setSource}
+                                {...form.getInputProps('source')}
                             />
                         </div>
-                    </div>
 
-                    <Button
-                        variant="filled"
-                        color="rgba(0, 0, 0, 1)"
-                        className='w-full'
-                        size="md"
-                        mt={10}
-                        radius="xl"
-                        onClick={handleSubmit}
-                    >
-                        <p className='text-xl'>Submit</p>
-                    </Button>
+                        <div className='w-full'>
+                            <Center>
+                                <Button
+                                    type="submit"
+                                    variant="filled"
+                                    color="rgba(0, 0, 0, 1)"
+                                    className="w-full"
+                                    size="md"
+                                    mt={10}
+                                    w={{ base: 275, sm: 275, lg: 475 }}
+                                    radius="xl"
+                                >
+                                    <p className="text-lg">Submit</p>
+                                </Button>
+                            </Center>
+                        </div>
+
+
+                    </form>
                 </div>
             </Modal>
+
+
         </div>
     );
 };
